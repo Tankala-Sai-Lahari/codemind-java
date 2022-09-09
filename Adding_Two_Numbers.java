902501c398c412042dc7f771611baa945859e5b2
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-09-09 11:19:11 +0530

    import java.util.Scanner; class Add{     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         System.out.println(a+b);         sc.close();     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Add{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+    }
+}
