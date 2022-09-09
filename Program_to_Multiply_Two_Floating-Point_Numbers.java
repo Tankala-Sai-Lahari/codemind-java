eb6fa6bdadc789348980c1c56c897254ebf66f41
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-09-09 11:47:43 +0530

    import java.util.Scanner; class flmul{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         float a,b;         a=sc.nextFloat();         b=sc.nextFloat();         System.out.format("%.2f",a*b);         sc.close();     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class flmul{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float a,b;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        System.out.format("%.2f",a*b);
+        sc.close();
+    }
+}
