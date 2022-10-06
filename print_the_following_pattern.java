6d1dddcde6a6a236bb84732642697a422010280b
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-10-06 20:38:10 +0530

    import java.util.Scanner; class Pattren{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++){             for(int j=0;j<n;j++){                 System.out.print((char)(65+i)+" ");             }             System.out.print(" ");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -3,13 +3,12 @@ class Pattren{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j+"");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print((char)(65+i)+" ");
             }
             System.out.print("
 ");
         }
-        sc.close();
     }
 }
